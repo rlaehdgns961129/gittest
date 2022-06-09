0fc119bf62cfc450c39ef74a4ed7d5e94fb52fa2
KDH <KDH@INNO-A-870>
2022-06-09 16:28:39 +0900

    bs_03 conflict()메소드 추가

--- gitTest/src/com/gittest/controller/FunctionalTest.java
+++ gitTest/src/com/gittest/controller/FunctionalTest.java
@@ -1,10 +1,14 @@
 package com.gittest.controller;
 
 public class FunctionalTest {
+	
 	public void printTest() {
 		String name = "test";
 	}
 	public String getGoodee() {
 		return "goodee";
+	}
+	public void conflict() {
+		System.out.println("충돌충돌");
 	}
 }
